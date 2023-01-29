20f5c4db7659f6297aa6901e6377ad37b60e4a48
lexapech <lexa.pech@yandex.ru>
2023-01-29 21:09:25 +0300

    add socket-chat

---
+++ socket-chat/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace sockettest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(411, 337);
+            this.panel1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(12, 392);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(330, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(348, 389);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 424);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(320, 200);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ socket-chat/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace sockettest
+{
+    public partial class Form1 : Form
+    {
+        
+        Server s;
+        Client client;
+
+        protected bool stopThread = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            s = new Server();
+            label1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s.StartServer();
+        }
+
+        class Client
+        {
+            public Socket socket;
+            public const int size = 1024;
+            public byte[] buffer;
+            Action<string> writeText;
+
+            public Client(Socket s)
+            {
+                socket = s;
+                buffer = new byte[size];
+            }
+            public Client(int port, Action<string> writeText)
+            {
+                IPAddress ipAddr = IPAddress.Any;
+
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+                socket = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                //socket.Bind(ipEndPoint);
+                buffer = new byte[size];
+                this.writeText = writeText;
+            }
+
+            public void Connect(IPEndPoint server)
+            {
+                writeText.Invoke("Connecting...\n");
+                try
+                {
+                    socket.Connect(server);
+                    writeText.Invoke("Connected\n");
+                    BeginReceive(Receive);
+                }
+                catch(SocketException ex)
+                {
+                    writeText.Invoke(ex.Message+"\n");
+                }
+            }
+
+            private void Receive(IAsyncResult result)
+            {
+                int bytesReceived = socket.EndReceive(result);
+                string data = Encoding.UTF8.GetString(buffer, 0, bytesReceived).Trim();
+                writeText.Invoke(data+"\n");
+                BeginReceive(Receive);
+            }
+            public void BeginReceive(AsyncCallback callback)
+            {
+                socket.BeginReceive(buffer, 0, size, SocketFlags.None, callback, this);
+            }
+
+            public void Send(string message)
+            {
+                if (!socket.Connected) writeText.Invoke("Not connected");
+                byte[] bytes = Encoding.UTF8.GetBytes(message);
+                socket.Send(bytes);
+            }
+        }
+
+        class Server
+        {
+            Thread listenThread;
+            List<Client> clients;
+            Socket sListener;
+            bool isRunning = false;
+            bool canAccept = false;
+
+            
+
+
+            public void StartServer()
+            {
+                if (isRunning) return;
+                canAccept = true;
+                isRunning = true;
+                IPAddress ipAddr = IPAddress.Any;
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 12345);
+                Console.WriteLine(ipAddr);
+                sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+                clients = new List<Client>();
+                listenThread = new Thread(listen);
+                Console.WriteLine("Starting thread");
+                listenThread.Start();
+            }
+            private void listen()
+            {
+                while (isRunning)
+                {
+                    if (canAccept)
+                    {
+                        Console.WriteLine("Begin accepting");
+                        sListener.BeginAccept(AcceptConnection, sListener);
+                        canAccept = false;
+                    }
+                    
+                }
+            }
+            public void stop()
+            {
+                if(listenThread!=null && listenThread.IsAlive)
+                    listenThread.Abort();
+            }
+            private void AcceptConnection(IAsyncResult result)
+            {
+                var client = new Client( sListener.EndAccept(result));
+                Console.WriteLine("{0} connected", client.socket.RemoteEndPoint);
+                client.BeginReceive(Receive);
+                byte[] message = Encoding.UTF8.GetBytes(String.Format("{0} joined the chat",client.socket.RemoteEndPoint));
+                foreach (Client c in clients)
+                {
+                    c.socket.Send(message, message.Length, SocketFlags.None);
+                }
+                clients.Add(client);
+                
+                Console.WriteLine("Clients: {0}", clients.Count);
+                canAccept = true;
+            }
+            private void Receive(IAsyncResult result)
+            {
+                var client = (Client)result.AsyncState;
+                if (!client.socket.Connected)
+                {
+                    Console.WriteLine("Client {0} disconnected", client.socket.RemoteEndPoint);
+                    client.socket.Close();
+                    clients.Remove(client);
+                    client.socket.Dispose();
+                    Console.WriteLine("Clients: {0}", clients.Count);
+                    return;
+                } 
+                    
+                int bytesReceived = client.socket.EndReceive(result);
+
+                if (bytesReceived == 0)
+                {
+                    Console.WriteLine("Client {0} disconnected", client.socket.RemoteEndPoint);
+                    
+                    client.socket.Close();
+                    clients.Remove(client);
+                    client.socket.Dispose();
+                    Console.WriteLine("Clients: {0}", clients.Count);
+                    return;
+                }
+                
+
+                string data = Encoding.UTF8.GetString(client.buffer, 0, bytesReceived).Trim();
+                Console.WriteLine("From {0} received {1}", client.socket.RemoteEndPoint, data);
+                byte[] message = Encoding.UTF8.GetBytes(String.Format("From {0} received {1}\n", client.socket.RemoteEndPoint, data));
+                foreach (Client c in clients)
+                {
+                    c.socket.Send(message, message.Length, SocketFlags.None);
+                }
+                client.BeginReceive(Receive);           
+            }
+
+        }
+   
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            s.stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            client = new Client(25123, AppendText);
+            try
+            {
+                client.Connect(new IPEndPoint(IPAddress.Parse(textBox1.Text), 12345));
+            }
+            catch(System.FormatException ex)
+            {
+                AppendText(ex.Message + "\n");
+            }
+        }
+        public void AppendText(string text)
+        {
+            Action<Label,string> d = delegate (Label l, string t) { l.Text = l.Text + t; };
+            BeginInvoke(d,label1,text);          
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (client == null) return;
+            client.Send(textBox2.Text);
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter) return;
+            if (client == null) return;
+            client.Send(textBox2.Text);
+        }
+    }
+}
